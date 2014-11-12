bc93e4f3acc502ab42b338c15e90ab8c2a6cd8c8
Mostafa Zamanian <zamanian.mcgill@gmail.com>
2014-11-11 22:52:26 -0600

    Update ros12.py

--- ros12.py
+++ ros12.py
@@ -22,11 +22,3 @@ print Evalue
 # d = Aa-Aa 0.75
 # e = Aa-aa 0.5
 # f = aa-aa 0
-
-#probability of at least one dominant allele = 1 - probability of homozygous recessive
-#odds of homozygous recessive: sum[P(nn)*1,P(mn)*(1/2),P(nm)*(1/2),P(mm)*(1/4)]
-#P(nn) = (n/t)*((n-1)/(t-1))
-#P(mn) = (m/t)*(n/(t-1)) * 1/2
-#P(nm) = (n/t)*(m/(t-1)) * 1/2
-#P(mm) = (m/t)*((m-1)/(t-1)) * 1/4
-#P(recessive) = ((n**2) - n + (m*n*0.5) / ((t**2) - t)
