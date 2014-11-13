024c1416032fc6813ca290ab88ceb9194dc8a264
Mostafa Zamanian <zamanian.mcgill@gmail.com>
2014-11-12 23:58:24 -0600

    Update ros14.py

--- ros14.py
+++ ros14.py
@@ -38,22 +38,16 @@ for i in range (0,num_seqs):
 	for j in range (i+1,num_seqs):
 		test_seq = Seqs[j]
 		tseq_len = len(test_seq)
-		hit_begin = re.match(curr_seq[0:k],test_seq[tseq_len-k:]) #match beginning to end
+		hit_begin = re.match(curr_seq[0:k],test_seq[tseq_len-k:])
 		if hit_begin and curr_seq != test_seq :
 			print SeqIDs[j], SeqIDs[i]
-			#print Seqs[j], Seqs[i]
-			#print 'curr_beg:', curr_seq[0:k], 'test_end:', test_seq[tseq_len-k:], "\n"
 		else:
 			pass
 		
-		hit_end = re.match(curr_seq[seq_len-k:seq_len],test_seq[0:k]) #match end to beginning
+		hit_end = re.match(curr_seq[seq_len-k:seq_len],test_seq[0:k])
 		if hit_end and curr_seq != test_seq:
 			print SeqIDs[i], SeqIDs[j]
-			#print Seqs[i], Seqs[j]
-			#print 'curr_end:', curr_seq[seq_len-k:seq_len], 'test_begin:', test_seq[0:k], "\n"
 		else:
 			pass
-
-
 
 f.close()
