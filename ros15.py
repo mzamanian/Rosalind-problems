66e9396cb432a9796e8240e3b0ad71a22e7ec4b6
Mostafa Zamanian <mzamanian@Mostafas-MacBook-Pro-2.local>
2014-11-12 23:58:06 -0600

    15

---
+++ ros15.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+import math
+from sys import argv 
+script, k, N = argv 
+
+k = int(k)
+N = int(N)
+
+def nCr(a,b):
+    f = math.factorial
+    return f(a) / f(b) / f(a-b)
+
+sum = 0
+for i in range(N,(2**k)+1):
+	print i
+	temp = nCr(2**k,i)*(0.25**(i))*(0.75**((2**k)-i))
+	sum = sum + temp
+print float(sum)
+
+
